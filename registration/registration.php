e616929ef1e147ece0749c7764bc43cfc4b9f829
monje <monje.pohl@gmai.com>
2026-05-05 14:25:35 +0200

    user registration email query lookup successfull

--- registration/registration.php
+++ registration/registration.php
@@ -3,6 +3,14 @@
 //include db connection file
 include '/Users/monje/PhpstormProjects/ecommerce/database/db_connection.php';
 
+$username = "";
+$email = "";
+$phone = "";
+$role = "";
+$hashed_password = "";
+
+$error_message_popup = "";
+
 // PHP registration file
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //validate and sanitize user input
@@ -19,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $row = $stmt->fetch();
 
     if ($row['counter'] >= 1){
-        echo "User already exists!";
+        $error_message_popup = "User already exists!";
     }else{
         //Seller Identification Check
         if ($role === 'seller') {
@@ -34,7 +42,69 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Welcome, Buyer! Registration successful.";
         }
     }
-} else {
-    // If someone tries to access this file directly without submitting the form
-    echo "Invalid request method.";
 }
+
+?>
+
+<!--creation of html registration page -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Registration</h1>
+    <h2>Create an Account</h2>
+    <form id="registration" action="registration.php" method="POST">
+        <div>
+            <label for="userName">Username: </label>
+            <input type="text" id="userName" name="userName" required value="<?php echo $username;?>">
+        </div>
+
+        <div>
+            <label for="email">Email: </label>
+            <input type="email" id="email" name="email" required value="<?php echo $email;?>">
+        </div>
+
+        <div>
+            <label for="phoneNumber">Phone Number: </label>
+            <input type="tel" id="phoneNumber" name="phoneNumber" required value="<?php echo $phone;?>">
+        </div>
+
+        <div>
+            <label for="role">I am registering as a: </label>
+            <select id="role" name="role">
+                <option value="buyer">Buyer</option>
+                <option value="seller">Seller</option>
+            </select>
+        </div>
+
+        <div>
+            <label for="password">Password: </label>
+            <input type="password" id="password" name="password" required>
+        </div>
+
+        <div>
+            <label for="confirmPassword">Confirm Password: </label>
+            <input type="password" id="confirmPassword" name="confirmPassword" required>
+        </div>
+
+        <button type="submit">Submit</button>
+    </form>
+
+    <script src="registration-validation.js"></script>
+
+    <?php
+    if ($error_message_popup != ""){
+        ?>
+
+        <script>
+            alert("<?php echo $error_message_popup;?>");
+        </script>
+
+        <?php
+    }
+    ?>
+</body>
