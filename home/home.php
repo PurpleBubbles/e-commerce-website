04e26fb7c8c9a9ae32e9ce3ad7ded36a00be85e8
monje <monje.pohl@gmai.com>
2026-05-13 17:14:02 +0200

    fixed home page product card styling

--- home/home.php
+++ home/home.php
@@ -3,8 +3,6 @@
 //include db connection file
 include '../database/db_connection.php';
 
-echo "Home Page";
-
 ?>
 
 <!DOCTYPE html>
@@ -12,126 +10,61 @@ echo "Home Page";
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/admin/style.css">
         <title>Home</title>
-
-        <style>
-            /* Style of sidebar */
-            .sidebar {
-                height: 100%;
-                width: 160px;
-                position: fixed;
-                top: 0;
-                left: 0;
-                background-color: orange;
-                overflow-x: hidden;
-                padding-top: 16px;
-            }
-
-            /* sidebar links */
-            .sidebar a {
-                padding: 6px 8px 6px 16px;
-                text-decoration: none;
-                font-size: 20px;
-                color: black;
-                display: block;
-            }
-
-            /* Style links on mouse-over */
-            .sidebar a:hover {
-                color: #f1f1f1;
-            }
-
-            container {
-                display: grid;
-                grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-                gap: 16px;
-            }
-
-            .card {
-                box-shadow: 0 4px 8px 0 orange;
-                max-width: 300px;
-                border-radius: 15px;
-                padding: 12px;
-                margin-left: 170px;
-            }
-
-            .price {
-    color: grey;
-    font-size: 22px;
-            }
-
-            img {
-                width: 100%;
-                height: 300px;
-                object-fit: cover;
-            }
-
-            .card button {
-                border: none;
-                outline: 0;
-                padding: 12px;
-                color: black;
-                background-color: orange;
-                text-align: center;
-                cursor: pointer;
-                width: 100%;
-                font-size: 18px;
-            }
-
-            .card button:hover {
-                opacity: 0.7;
-            }
-        </style>
 
     </head>
     <body>
-    <!-- Load an icon library -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <!-- sidebar -->
-    <div class="sidebar">
-        <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
-        <a href="#services"><i class="fa fa-fw fa-wrench"></i> Buy</a>
-        <a href="#clients"><i class="fa fa-fw fa-user"></i> Sell</a>
-        <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
-    </div>
+    <header>
+        <div class="home_page">
+            <label >
+                <h3>Home</h3>
+            </label>
+        </div>
 
-    <div class="content">
-        <div class="container">
-            <div class="card">
-                <img src="/media/coffee.jpeg" alt="White coffee mug and saucer on a table ">
-                <h2>Coffee Mug</h2>
-                <p class="price">R20.00</p>
-                <p>A description of the product</p>
-                <p>
-                    <button>View</button>
-                    <button>Buy</button>
-                </p>
+        <div class="notifications">
+            <div class="circle"></div>
+            <img src="/media/notification.png" class="icn" alt="notification alert" />
+            <div class="dp">
+                <img src="/media/profile.png" class="dpicn" alt="profile emoticon" />
             </div>
-            <div class="card">
-                <img src="/media/coffee.jpeg" alt="White coffee mug and saucer on a table ">
-                <h2>Coffee Mug</h2>
-                <p class="price">R20.00</p>
-                <p>A description of the product</p>
-                <p>
-                    <button>View</button>
-                    <button>Buy</button>
-                </p>
-            </div>
-            <div class="card">
-                <img src="/media/coffee.jpeg" alt="White coffee mug and saucer on a table ">
-                <h2>Coffee Mug</h2>
-                <p class="price">R20.00</p>
-                <p>A description of the product</p>
-                <p>
-                    <button>View</button>
-                    <button>Buy</button>
-                </p>
+        </div>
+    </header>
+
+    <div class="main-container">
+        <div class="navcontainer">
+
+            <nav class="navigation_bar">
+                <div class="nav-upper-options">
+
+                    <div class="nav-option Home" onclick="location.href='/home/home.php'">
+                        <img src="/media/home.png" class="report-img" alt="home" />
+                        <h3>Home</h3>
+                    </div>
+                    <div class="nav-option Logout" onclick="location.href='/logn/login.php'">
+                        <img src="/media/logout.png" class="report-img" alt="logout" />
+                        <h3>Logout</h3>
+                    </div>
+
+                </div>
+            </nav>
+
+        </div>
+
+        <div class="main">
+            <div class="box-products">
+                <div class="product">
+                    <img src="/media/coffee.jpeg" class="product-img" alt="coffee" />
+                    <div class="product-text">
+                        <h2 class="topic-heading">Product name</h2>
+                        <h2 class="topic">Product price</h2>
+                        <h2 class="topic">Location</h2>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
-
-
     </body>
 </html>
