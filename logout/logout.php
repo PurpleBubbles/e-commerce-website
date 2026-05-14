d2cba5299a223055095868baa4badad6a9ed227b
monje <monje.pohl@gmai.com>
2026-05-14 20:18:42 +0200

    Make button on logout page to redirect to login page should user want to login.

--- logout/logout.php
+++ logout/logout.php
@@ -7,3 +7,19 @@ session_unset();
 session_destroy();
 
 echo "Logged out successfully.";
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+</head>
+
+<body>
+    <button class="view" onclick="location.href='/login/login.php'">Go to login page</button>
+
+</body>
+
+</html>
