f89a03a5827e18fb3fb0d2a43bb77e4cf78047b6
monje <monje.pohl@gmai.com>
2026-05-21 11:51:05 +0200

    Show images on home page if available, otherwise display error image. Of buyer pages

--- buyer/home.php
+++ buyer/home.php
@@ -71,7 +71,7 @@ $rows = $stmt->fetchAll();
                 foreach($rows as $row){
                     //check if product has been sold prior to displaying
                     if($row['status'] !== 0){
-                        echo ProductCtrl::displayProduct($row);
+                        echo ProductCtrl::displayProduct($conn, $row);
                     }
                 }
             ?>
