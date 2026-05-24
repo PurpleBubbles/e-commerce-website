fad3b5c7f49c45c39917c3fac4240068806ace43
monje <monje.pohl@gmai.com>
2026-05-24 18:59:57 +0200

    login page now has pretty design

--- login/login.php
+++ login/login.php
@@ -66,100 +66,71 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/main.css" rel="stylesheet" />
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
     <title>Login</title>
-    <style>
-        /* General page styling */
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f0f2f5;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 10px;
-        }
-
-        h1 {
-            font-size: 3rem;
-            text-align: center;
-            margin-bottom: 10px;
-        }
-
-        h2 {
-            text-align: center;
-            color: #333;
-            margin-bottom: 24px;
-        }
-
-        /* Button styling with hover effect */
-        btn {
-            width: 100%;
-            padding: 12px;
-            background-color: #04AA6D;
-            text-align: center;
-            justify-content: center;
-            color: white;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            font-size: 16px;
-            transition: background-color 0.3s ease;
-        }
-
-        btn:hover {
-            background-color: #038d5a;
-        }
-
-        label {
-            display: block;
-            width: 100%;
-            text-align: left;
-        }
-        input {
-            width: 100%;
-            margin-top: 5px;
-            margin-bottom: 5px;
-            background-color: orange;
-            text-align: center;
-            justify-content: center;
-            color: white;
-        }
-
-        /* Footer links */
-        .footer-links {
-            margin-top: 20px;
-            text-align: center;
-            font-size: 14px;
-        }
-
-        .footer-links a {
-            color: #007bff;
-            text-decoration: none;
-        }
-    </style>
 </head>
+
 <body>
-<h1>Login</h1>
-<h2>Sign-in to Account</h2>
-<form id="login" action="login.php" method="POST">
-    <div>
-        <label for="email">Email: </label>
-        <input type="email" id="email" name="email" required value="<?php echo $email;?>">
-    </div>
 
-    <div>
-        <label for="password">Password: </label>
-        <input type="password" id="password" name="password" required >
-    </div>
 
-    <button type="submit">Login</button>
-</form>
-<a href="/registration/buyer_registration.php" class="footer-links">Register as Buyer</a>
-<a href="/registration/seller_registration.php" class="footer-links">Register as Seller</a>
+<section class="vh-100" style="background-color: #253952;">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-lg-12 col-xl-11">
+                <div class="card text-black" style="border-radius: 25px;">
+                    <div class="card-body p-md-5">
+                        <div class="row justify-content-center">
+                            <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login to Account</p>
+
+                                <form id="login" action="login.php" method="POST" class="mx-1 mx-md-4">
+
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                        <div>
+                                            <input type="email" id="email" name="email" required value="<?php echo $email;?>">
+                                            <label for="email">Email</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div>
+                                            <input type="password" id="password" name="password" required >
+                                            <label for="password">Password</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                        <button type="button" class="btn btn-primary btn-lg">Login</button>
+                                    </div>
+
+                                </form>
+
+                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                                    <a href="/registration/buyer_registration.php" class="footer-links">Register as Buyer</a>
+
+                                </div>
+
+                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                                    <a href="/registration/seller_registration.php" class="footer-links">Register as Seller</a>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 if ($error_message_popup != ""){
