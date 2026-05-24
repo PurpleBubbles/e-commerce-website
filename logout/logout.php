06a505adfc3d2db0955e29c4a0d1281ed518b239
monje <monje.pohl@gmai.com>
2026-05-24 18:32:56 +0200

    logout page now has pretty design

--- logout/logout.php
+++ logout/logout.php
@@ -7,8 +7,6 @@ session_unset();
 
 // Destroy the session
 session_destroy();
-
-echo "Logged out successfully.";
 ?>
 
 <!DOCTYPE HTML>
@@ -16,12 +14,46 @@ echo "Logged out successfully.";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/main.css" rel="stylesheet" />
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
     <title>Logout</title>
 </head>
 
 <body>
-    <button class="view" onclick="location.href='/login/login.php'">Go to login page</button>
+    <section class="vh-100" style="background-color: #253952;">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-lg-12 col-xl-11">
+                <div class="card text-black" style="border-radius: 25px;">
+                    <div class="card-body p-md-5">
+                        <div class="row justify-content-center">
+                            <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
+                                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Successfully Logged Out</p>
+
+                                <form class="mx-1 mx-md-4">
+
+                                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                        <button type="button" class="btn btn-primary btn-lg" onclick="location.href='/login/login.php'" >To Login Page</button>
+                                    </div>
+
+                                </form>
+
+                            </div>
+                            <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                                <img src="/media/logout_illustration.png"
+                                     class="img-fluid" alt="Sample image">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </body>
 
 </html>
