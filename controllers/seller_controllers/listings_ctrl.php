4fef38334f7497e3ba6a497b1081cafd3b0ce921
monje <monje.pohl@gmai.com>
2026-05-25 23:00:25 +0200

    seller created user listings page and controller for displaying user's listings

---
+++ controllers/seller_controllers/listings_ctrl.php
@@ -0,0 +1,47 @@
+<?php
+class ListingsCtrl {
+    public static function displayListings($conn, $row): string {
+
+        $product_id = $row['product_id'];
+
+        $sql = "SELECT * FROM PRODUCT_IMAGES WHERE product_id = ? LIMIT 1";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$product_id]);
+        $rows = $stmt->fetch();
+
+        if (empty($rows)) {
+            return <<<HTML
+            <div class="col col-sm-4 col-lg-4 my-3">
+                <div class="h-100 card text-black " style="border-radius: 10px">
+                    <img style="max-height: 300px;" src="/media/image-break.png" class="card-img-top object-fit-cover p-1" alt="broken" />
+                    <div class="card-body p-md-3">
+                        <div class=justify-content-center">
+                            
+                            <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">{$row['product_name']}</p>
+                            <p class="text-start h2 bold">R{$row['price']}</p>
+                            <p class="text-center">{$row['description']}</p>                          
+                        </div>
+                    </div>
+                </div>
+            </div>
+            HTML;
+        } else{
+            $image_row = $rows;
+            return <<<HTML
+            <div class="col col-sm-4 col-lg-4 my-3">
+                <div class="h-100 card text-black " style="border-radius: 10px">
+                    <img style="max-height: 300px;" src="/image.php?image_id={$image_row['image_id']}" class="card-img-top object-fit-cover p-1" alt="broken" />
+                    <div class="card-body p-md-3">
+                        <div class=justify-content-center">
+                            
+                            <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">{$row['product_name']}</p>
+                            <p class="text-start h2 bold">R{$row['price']}</p>
+                            <p class="text-center">{$row['description']}</p>                          
+                        </div>
+                    </div>
+                </div>
+            </div>
+            HTML;
+        }
+    }
+}
--- seller/user_listings.php
+++ seller/user_listings.php
@@ -1,2 +1,114 @@
 <?php
-echo "user_listings";
+//start user session
+session_start();
+include '../controllers/seller_controllers/listings_ctrl.php';
+
+//include db connection file
+include '../database/db_connection.php';
+$user_id = $_SESSION['user_id'];
+
+//get all of users listed products
+$sql = "SELECT * FROM PRODUCTS WHERE seller_user_id = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute([$user_id]);
+$rows = $stmt->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/main.css" rel="stylesheet" />
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <title>Listings</title>
+
+</head>
+<body style="height: 100vh;">
+
+<header class="p-3 bg-primary text-white">
+
+    <div class="container">
+        <div lass="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/seller/home.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                My Listings
+            </a>
+        </div>
+    </div>
+</header>
+
+<main class="h-100 d-flex flex-nowrap flex-fill">
+    <div class="h-100 d-flex flex-column flex-shrink-0 p-3 text-bg-secondary" style="width: auto;">
+        <a href="/seller/user_listings.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <img width="40px" src="/media/logo.png" alt="logo" />
+            <span class="fs-4 text-black">Product Name</span>
+        </a>
+        <ul class="nav nav-pills flex-column mb-auto">
+
+            <li class="nav-item my-2">
+                <a href="/seller/home.php" class="nav-link active" aria-current="page">
+                    <img width="20px" src="/media/home.png" class="report-img text-black" alt="home"/>
+                    Home
+                </a>
+            </li>
+            <li class="nav-item my-2">
+                <a href="/seller/bought.php" class="nav-link active" aria-current="page">
+                    <img width="20px" src="/media/bought.png" class="report-img" alt="Bought items"/>
+                    Bought
+                </a>
+            </li>
+            <li class="nav-item my-2">
+                <a href="/seller/sold.php" class="nav-link active" aria-current="page">
+                    <img width="20px" src="/media/sold.png" class="report-img" alt="Sold items"/>
+                    Sold
+                </a>
+            </li>
+            <li class="nav-item my-2">
+                <a href="/seller/user_listings.php" class="nav-link active" aria-current="page">
+                    <img width="20px" src="/media/collection.png" class="report-img" alt="My Listings"/>
+                    My Listings
+                </a>
+            </li>
+            <li class="nav-item my-2">
+                <a href="/seller/listing.php" class="nav-link active" aria-current="page">
+                    <img width="20px" src="/media/list.png" class="report-img" alt="List Product"/>
+                    List
+                </a>
+            </li>
+            <li class="nav-item my-2">
+                <a href="/logout/logout.php" class="nav-link active" aria-current="page">
+                    <img width="20px" src="/media/logout.png" class="report-img" alt="home"/>
+                    Logout
+                </a>
+            </li>
+
+        </ul>
+    </div>
+
+    <div class="container-lg my-1">
+        <div class="row">
+
+            <?php
+            echo '<div>';
+
+            $count = 0;
+            foreach($rows as $row){
+
+                if ($count % 3 == 0) {
+                    echo '</div><div class="row">';
+                }
+                $count++;
+
+                echo ListingsCtrl::displayListings($conn, $row);
+            }
+            echo '</div>';
+
+            ?>
+
+        </div>
+    </div>
+</main>
+</body>
+</html>
