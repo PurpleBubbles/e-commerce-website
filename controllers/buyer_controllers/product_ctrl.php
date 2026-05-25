600b8b8984be4f5386cb696e119c933e94b5c1c2
monje <monje.pohl@gmai.com>
2026-05-25 15:55:14 +0200

    bought page showing 3 cards at a time now with images and buttons better fitted only of buyer pages

--- controllers/buyer_controllers/product_ctrl.php
+++ controllers/buyer_controllers/product_ctrl.php
@@ -12,7 +12,7 @@ class ProductCtrl {
             return <<<HTML
             <div class="col col-sm-4 col-lg-4 my-3">
                 <div class="h-100 card text-black " style="border-radius: 10px">
-                    <img style="max-height: 300px;" src="/media/image-break.png" class="card-img-top object-fit-cover p-1" alt="coffee" />
+                    <img style="max-height: 300px;" src="/media/image-break.png" class="card-img-top object-fit-cover p-1" alt="broken" />
                     <div  class="card-body p-md-3">
                         <div class=justify-content-center">
                             
