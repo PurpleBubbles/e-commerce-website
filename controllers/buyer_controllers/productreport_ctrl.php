aad2749054f1ed23004e051fbe474a9aca7040ab
monje <monje.pohl@gmai.com>
2026-05-25 20:31:36 +0200

    product reporting page now semi pretty and report functionality is working

--- controllers/buyer_controllers/productreport_ctrl.php
+++ controllers/buyer_controllers/productreport_ctrl.php
@@ -1,28 +1,22 @@
 <?php
 class ProductReportCtrl {
-    public static function displayReportedProduct($row): string {
+    public static function displayReportedProduct($row, $row_image): string {
         return <<<HTML
-        <p>
-        <h2 class="topic-heading">Reported Product</h2>
-        </p>
-        <form method="POST">
-        <div class="box-products">
-            <div class="product">
-                <img src="/media/coffee.jpeg" class="product-img" alt="coffee" />
-                <div class="product-text">
-                    <h2 class="topic-heading">{$row['product_name']}</h2>
-                    <h2 class="topic">R{$row['price']}</h2>
-                    <h2 class="topic">{$row['description']}</h2>
+        <div class="col col-sm-4 col-lg-4 my-3">
+            <div class="h-100 card text-black " style="border-radius: 10px">
+                <img style="max-height: 300px;" src="/image.php?image_id={$row_image['image_id']}" class="card-img-top object-fit-cover p-1" alt="broken"/>
+                <div class="card-body p-md-3">
+                    <div class=justify-content-center">
+                        <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">{$row['product_name']}</p>
+                        <p class="text-start h2 bold">R{$row['price']}</p>
+                        <p class="text-center">{$row['description']}</p>                       
+                    </div>
                 </div>
+                
             </div>
+            
         </div>
-
-        <div>
-            <label for="report">Report Reason</label>  
-            <textarea name="report" id="report" cols="30" rows="10" required placeholder="Enter the reason for your report here..."></textarea>      
-        </div>
-        <button class="view" type="submit">Report Product</button>
-        </form>
+        
         HTML;
     }
 }
